94ccb037ea3662437c01c64efdb86df3f8f5838f
maxono1 <92861475+maxono1@users.noreply.github.com>
2023-01-12 12:17:14 +0100

    feat: fertig mit dem ganzen scheiss projekt alter

---
+++ VTS-Projektarbeit/VTS-Projektarbeit/src/main/java/hsos/vts/Adresse.java
@@ -0,0 +1,13 @@
+package hsos.vts;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Adresse extends PanacheEntity {
+    public String plz;
+}

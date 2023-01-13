f860f3755375dc05e3dd8b68ffb4e268a86880c8
maxono1 <92861475+maxono1@users.noreply.github.com>
2023-01-13 10:57:17 +0100

    feat: datenbank startet und stoppt clean

--- VTS-Projektarbeit/VTS-Projektarbeit/src/main/java/hsos/vts/Main.java
+++ VTS-Projektarbeit/VTS-Projektarbeit/src/main/java/hsos/vts/Main.java
@@ -6,14 +6,18 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @QuarkusMain
 public class Main {
     public static EmbeddedPostgres pg;
     public static void main(String ... args) {
         System.out.println("Running main method");
+        System.out.println("Present Project Directory : " + System.getProperty("user.dir"));
+        //Path databaseFiles = Paths.get(System.getProperty("user.dir")+ "\\databaseFiles");
         try {
-            pg = EmbeddedPostgres.builder().setPort(6972).start();
+            pg = EmbeddedPostgres.builder().setPort(6973).start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -24,9 +28,9 @@ public class Main {
 
         @Override
         public int run(String... args) throws Exception {
-            System.out.println("Do startup logic here");
             Quarkus.waitForExit();
             pg.close();
+            System.out.println("datenbank erfolgreich beendet");
             return 0;
         }
     }

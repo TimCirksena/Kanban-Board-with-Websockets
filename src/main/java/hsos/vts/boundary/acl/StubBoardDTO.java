593b576d305bfb6a4bb3675ea828051a359c3fdc
maxono1 <92861475+maxono1@users.noreply.github.com>
2023-01-14 14:20:46 +0100

    feat: StubboardDTO constructor

--- src/main/java/hsos/vts/boundary/acl/StubBoardDTO.java
+++ src/main/java/hsos/vts/boundary/acl/StubBoardDTO.java
@@ -4,6 +4,8 @@ public class StubBoardDTO {
     public long boardId;
     public String titel;
 
+    public StubBoardDTO(){}
+
     public StubBoardDTO(long boardId, String titel){
         this.boardId = boardId;
         this.titel = titel;

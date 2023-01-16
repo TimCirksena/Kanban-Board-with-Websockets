e71dddc7ef285b9849253e561582734cdc5de490
maxono1 <92861475+maxono1@users.noreply.github.com>
2023-01-16 10:52:14 +0100

    feat: simples both way drag and drop implementiert

--- src/main/java/hsos/vts/boundary/rest/ElementKanbanResource.java
+++ src/main/java/hsos/vts/boundary/rest/ElementKanbanResource.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class ElementKanbanResource {
 
     @Inject
-    private ElementKanbanCatalog elementKanbanCatalog;
+    ElementKanbanCatalog elementKanbanCatalog;
 
     @GET
     @Transactional

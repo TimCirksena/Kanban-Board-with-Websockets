294ad47caffdc284944bc98dd23eb50460f1f646
TimCirksena <tim.cirksena@outlook.de>
2023-01-20 14:17:40 +0100

    feat: modal bei board_view hinzugefügt

--- src/main/resources/META-INF/resources/scripts/allBoards_script.js
+++ src/main/resources/META-INF/resources/scripts/allBoards_script.js
@@ -111,4 +111,37 @@ document.getElementById("submit-kanban").addEventListener("click", function (eve
         .catch(function (error) {
             alert("Fehler bei erstellen des Kanban boards: " + error);
         });
+    // Close the modal
+    modal.style.display = "none";
 });
+document.getElementById("open-modal-btn").addEventListener("click", function (event){
+
+    var modal = document.getElementById("modal");
+    console.log(modal);
+// Get the button that opens the modal
+    var btn = document.getElementById("open-modal-btn");
+    console.log(btn);
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-modal")[0];
+
+// When the user clicks the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+// Get the form element
+    var form = document.getElementById("modal-form");
+
+});
+
